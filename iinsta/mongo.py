0a74b00835fa74dbd61bf7b6d852c234e3d22452
Sebastian karlsson <sebbekarlsson97@gmail.com>
2018-04-19 13:00:21 +0200

    added mongo

---
+++ iinsta/mongo.py
@@ -0,0 +1,10 @@
+from iinsta.config import config
+from pymongo import MongoClient
+from mongoengine import connect
+
+
+client = MongoClient(config['mongo']['host'])
+DEFAULT_CONNECTION = connect(config['mongo']['db'])
+
+
+db = client[config['mongo']['db']]
