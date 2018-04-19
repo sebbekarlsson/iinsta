f3eec279accfdc35d6c29c9f44c84647ac5d7379
Sebastian karlsson <sebbekarlsson97@gmail.com>
2018-04-19 14:46:21 +0200

    created ProfileEditForm

---
+++ iinsta/forms/ProfileEditForm.py
@@ -0,0 +1,10 @@
+from wtforms import Form, StringField, validators, BooleanField, FileField
+
+
+class ProfileEditForm(Form):
+    firstname = StringField('Firstname', [validators.Length(min=3, max=120)])
+    lastname = StringField('Lastname', [validators.Length(min=3, max=120)])
+    website = StringField('Website', [validators.Length(max=250)])
+    bio = StringField('Biography', [validators.Length(max=140)])
+    private = BooleanField('Private')
+    avatar = FileField('Private')
--- iinsta/views/account.py
+++ iinsta/views/account.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, request
 from iinsta.session_utils import login_required
+from iinsta.forms.ProfileEditForm import ProfileEditForm
 
 
 bp = Blueprint(__name__, __name__, template_folder='templates')
@@ -15,7 +16,13 @@ def show(account_name):
     return render_template('account.html')
 
 
-@bp.route('/<account_name>/edit')
+@bp.route('/<account_name>/edit', methods=['POST', 'GET'])
 @login_required
 def show_edit(account_name):
-    return render_template('account_edit.html')
+    errors = []
+    form = ProfileEditForm(request.form)
+
+    if request.method == 'POST' and form.validate():
+        pass
+
+    return render_template('account_edit.html', form=form, errors=errors)
