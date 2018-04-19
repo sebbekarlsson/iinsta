b1235f4b71a96cb863c37804752c20f4feef3a5a
Sebastian karlsson <sebbekarlsson97@gmail.com>
2018-04-19 13:01:19 +0200

    added UserFacade

---
+++ iinsta/facades/UserFacade.py
@@ -0,0 +1,36 @@
+from iinsta.entities.User import User
+from mongoengine.queryset import DoesNotExist
+from iinsta.mongo import db
+
+
+class UserFacade(object):
+    @staticmethod
+    def get_by_ids(ids):
+        return db.page.find({
+            '_id': {'$in': ids}
+        })
+
+    @staticmethod
+    def get(**kwargs):
+        try:
+            return User.objects.get(**kwargs)
+        except DoesNotExist:
+            return None
+
+    @staticmethod
+    def get_all():
+        return User.objects().order_by('name')
+
+    @staticmethod
+    def create(**kwargs):
+        c = User(**kwargs)
+        c.save()
+
+        return c
+
+    @staticmethod
+    def exists(**kwargs):
+        try:
+            return User.objects.get(**kwargs) is not None
+        except DoesNotExist:
+            return False
