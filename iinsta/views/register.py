8cbf58076ee1529af230e3eb5352f3d255a6685d
Sebastian karlsson <sebbekarlsson97@gmail.com>
2018-04-19 13:06:22 +0200

    be able to register account

--- iinsta/views/register.py
+++ iinsta/views/register.py
@@ -1,5 +1,7 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, session, redirect
+from iinsta.password import get_hashed_password
 from iinsta.forms.RegisterForm import RegisterForm
+from iinsta.facades.UserFacade import UserFacade
 
 
 bp = Blueprint(
@@ -12,9 +14,22 @@ bp = Blueprint(
 
 @bp.route('/', methods=['POST', 'GET'])
 def show():
+    errors = []
     form = RegisterForm(request.form)
 
     if request.method == 'POST' and form.validate():
-        print('form')
+        if UserFacade.exists(name=form.name.data):
+            errors.append('User already exists')
+        else:
+            user = UserFacade.create(
+                name=form.name.data,
+                firstname=form.firstname.data,
+                lastname=form.lastname.data,
+                password=get_hashed_password(form.password.data)
+            )
 
-    return render_template('register.html', form=form)
+            session['user_id'] = str(user.id)
+
+            return redirect('/')
+
+    return render_template('register.html', form=form, errors=errors)
