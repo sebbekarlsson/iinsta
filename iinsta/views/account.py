bef9ed1803987ecfb1c365a3ab84b26a0f76440c
Sebastian karlsson <sebbekarlsson97@gmail.com>
2018-04-19 13:18:31 +0200

    login required for account

--- iinsta/views/account.py
+++ iinsta/views/account.py
@@ -1,10 +1,12 @@
 from flask import Blueprint, render_template, request
+from iinsta.session_utils import login_required
 
 
 bp = Blueprint(__name__, __name__, template_folder='templates')
 
 
 @bp.route('/<account_name>', methods=['POST', 'GET'])
+@login_required
 def show(account_name):
     if request.method == 'POST':
         if request.form.get('follow'):
