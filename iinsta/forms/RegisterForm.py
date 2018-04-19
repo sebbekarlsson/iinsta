97cebd022b453f52e1b25d26eeef17b4ed131c9c
Sebastian karlsson <sebbekarlsson97@gmail.com>
2018-04-19 12:52:54 +0200

    registration design fix

--- iinsta/forms/RegisterForm.py
+++ iinsta/forms/RegisterForm.py
@@ -5,7 +5,8 @@ class RegisterForm(Form):
     name = StringField('Username', [validators.Length(min=4, max=120)])
     firstname = StringField('Firstname', [validators.Length(min=3, max=120)])
     lastname = StringField('Lastname', [validators.Length(min=3, max=120)])
-    password = PasswordField('New Password', [
+    password = PasswordField('Password', [
         validators.DataRequired(),
         validators.EqualTo('confirm', message='Passwords must match')
     ])
+    confirm = PasswordField('Confirm Password')
