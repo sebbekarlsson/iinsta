d181acd6deda3c564f8444ac58fe2094aec5101e
Sebastian karlsson <sebbekarlsson97@gmail.com>
2018-04-19 14:14:34 +0200

    added private, bio and website field to User

--- iinsta/entities/User.py
+++ iinsta/entities/User.py
@@ -1,4 +1,4 @@
-from mongoengine import Document, StringField, ReferenceField
+from mongoengine import Document, StringField, ReferenceField, BooleanField
 from iinsta.entities.Asset import Asset
 
 
@@ -8,3 +8,6 @@ class User(Document):
     lastname = StringField(max_length=120)
     password = StringField(min_length=5)
     avatar = ReferenceField(Asset)
+    private = BooleanField(default=False)
+    bio = StringField(max_length=140)
+    website = StringField(max_length=250)
