e9db5242c04f2d2243dbec801af1a44bd2b0d641
Sebastian karlsson <sebbekarlsson97@gmail.com>
2018-04-20 13:41:39 +0200

    be able to tag your photos using hashtags

--- iinsta/entities/Article.py
+++ iinsta/entities/Article.py
@@ -17,7 +17,14 @@ class Article(Document):
     user = ReferenceField(User, required=True)
     likers = ListField(ReferenceField(User))
     comments = ListField(ReferenceField(Comment))
+    tags = ListField(StringField(max_length=140))
     created_at = DateTimeField(default=datetime.datetime.now)
+
+    def save(self, *args, **kwargs):
+        for tag in self.get_tags_from_content():
+            self.tags.append(tag.replace('#', ''))
+
+        return super(Article, self).save(*args, **kwargs)
 
     def like(self, user):
         if user not in self.likers:
@@ -34,3 +41,16 @@ class Article(Document):
         comment.save()
         self.comments.append(comment)
         self.save()
+
+    def get_tags_from_content(self):
+        return filter(lambda x: '#' in x, self.content.lower().split(' '))
+
+    def get_rendered_content(self):
+        words = self.content.split(' ')
+
+        for i, word in enumerate(words):
+            if word.replace('#', '') in self.tags:
+                words[i] = '<a href="/feed/tag/{}">{}</a>'\
+                    .format(word.replace('#', ''), word)
+
+        return ' '.join(words)
